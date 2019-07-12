080d2e23e7d96035d283ab52653a8a74235e8d71
Ganing <12764014+LeeGaning@users.noreply.github.com>
2019-07-12 14:36:08 +0800

    add appveyor build config file

---
+++ build.sh
@@ -0,0 +1,17 @@
+export LLVM_ROOT=c:/projects/clang-experimental
+cd $LLVM_ROOT
+echo "Cwd =" `pwd`
+mkdir build
+cd build
+export CC=cl.exe
+export CXX=cl.exe
+cmake -G "CodeBlocks - Unix Makefiles"  -DCMAKE_BUILD_TYPE=Release c:/projects/clang-experimental/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DPYTHON_EXECUTABLE:FILEPATH=c:\\Python27\\python.exe
+
+export PROJ_ROOT=c:/projects/clang-experimental/build
+echo "Cwd =" `pwd`
+make clang-format
+echo `pwd`
+cd $PROJ_ROOT
+echo `pwd`
+cp $PROJ_ROOT/bin/clang-format.exe $PROJ_ROOT/bin/clang-format-experimental.exe
+zip $PROJ_ROOT/clang-format-experimental.zip $PROJ_ROOT/bin/clang-format-experimental.exe
