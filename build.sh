135ebcefe82e7d26a907b9010569dd8ad90725d5
Ganing <12764014+LeeGaning@users.noreply.github.com>
2019-07-12 15:52:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,5 +13,4 @@ make clang-format
 echo `pwd`
 cd $PROJ_ROOT
 echo `pwd`
-cp $PROJ_ROOT/bin/clang-format.exe $PROJ_ROOT/bin/clang-format-experimental.exe
-zip $PROJ_ROOT/clang-format-experimental.zip $PROJ_ROOT/bin/clang-format-experimental.exe
+zip $PROJ_ROOT/clang-format-experimental.zip $PROJ_ROOT/bin/clang-format.exe
